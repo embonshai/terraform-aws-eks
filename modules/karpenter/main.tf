82a0cd21f9e357b7a0c82c92742948e3a84d5bd6
embonshai <109651023+embonshai@users.noreply.github.com>
2024-10-28 09:06:54 +0200

    Update main.tf

--- modules/karpenter/main.tf
+++ modules/karpenter/main.tf
@@ -4,7 +4,6 @@ data "aws_caller_identity" "current" {}
 
 locals {
   account_id = data.aws_caller_identity.current.account_id
-  dns_suffix = data.aws_partition.current.dns_suffix
   partition  = data.aws_partition.current.partition
   region     = data.aws_region.current.name
 }
